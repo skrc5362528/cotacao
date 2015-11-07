772b6fa6ecda849a8d7e63e68ddd982e7a3b4b21
vitorcastro78@gmail.com <lara2308>
2015-11-07 12:39:46 -0200

    sdsd

--- www/scripts/paginas/mapa.js
+++ www/scripts/paginas/mapa.js
@@ -1,22 +1,22 @@
-﻿document.addEventListener("deviceready", onDeviceReady, false);
-var optionsWatchPosition = { frequency: 3000, enableHighAccuracy: true};
+﻿//document.addEventListener("deviceready", onDeviceReady, false);
+//var optionsWatchPosition = { frequency: 3000, enableHighAccuracy: true};
 
-function success(pos) {
-    var crd = pos.coords;
-    localStorage.setItem('latitude', crd.latitude);
-    localStorage.setItem('longitude', crd.longitude);
+//function success(pos) {
+//    var crd = pos.coords;
+//    localStorage.setItem('latitude', crd.latitude);
+//    localStorage.setItem('longitude', crd.longitude);
 
-   // alert(crd.latitude, crd.longitude);
-};
+//   // alert(crd.latitude, crd.longitude);
+//};
 
-function error(err) {
-    alert('ERROR(' + err.code + '): ' + err.message);
-};
+//function error(err) {
+//    alert('ERROR(' + err.code + '): ' + err.message);
+//};
 
-function onDeviceReady() {
-    navigator.geolocation.watchPosition(success, error, optionsWatchPosition);
-}
-    //navigator.geolocation.getCurrentPosition(success, error, optionsWatchPosition);
-    //cordova.plugins.locationServices.geolocation.watchPosition(success, error, optionsWatchPosition);
-    //alert('tentativa location services');
-    //var optionsGetCurrentPosition = { maximumAge: 3000, enableHighAccuracy: true};
+//function onDeviceReady() {
+//    navigator.geolocation.watchPosition(success, error, optionsWatchPosition);
+//}
+//    //navigator.geolocation.getCurrentPosition(success, error, optionsWatchPosition);
+//    //cordova.plugins.locationServices.geolocation.watchPosition(success, error, optionsWatchPosition);
+//    //alert('tentativa location services');
+//    //var optionsGetCurrentPosition = { maximumAge: 3000, enableHighAccuracy: true};
