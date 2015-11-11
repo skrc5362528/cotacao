fb2c14b8184e5c93a07dfd62b0e57a8c0f9e2d0b
Vitor Castro <vitorcastro78@gmail.com>
2015-11-11 18:04:49 -0200

    wswsw

--- www/scripts/paginas/index.js
+++ www/scripts/paginas/index.js
@@ -14,3 +14,23 @@ function PreparaSistema(pagina) {
 }
 
 
+document.addEventListener("deviceready", onDeviceReady, false);
+var optionsWatchPosition = { frequency: 3000, enableHighAccuracy: true };
+//var optionsGetCurrentPosition = { maximumAge: 3000, enableHighAccuracy: true };
+
+function success(pos) {
+
+    var crd = pos.coords;
+
+        localStorage.setItem('latitude', crd.latitude);
+        localStorage.setItem('longitude', crd.longitude);
+};
+
+function error(err) {
+    alert('ERROR(' + err.code + '): ' + err.message);
+};
+
+function onDeviceReady() {
+    navigator.geolocation.watchPosition(success, error, optionsWatchPosition);
+    //navigator.geolocation.getCurrentPosition(success, error, optionsWatchPosition);
+}
--- www/scripts/paginas/map.js
+++ www/scripts/paginas/map.js
@@ -1,30 +1,34 @@
-﻿function BuscaMapa() {
-    var Long = sessionStorage.getItem('longitude');
-    var Lat = sessionStorage.getItem('latitude');
-    if (Long!= '' &&  Lat!= '') {
-        IniciaMapa(Lat, Long);
-    }
-    else {
-        alert('Não há localização disponível');
-    }
+﻿
+function BuscaMapa()
+{
+    AtualizaMapa(localStorage.latitude, localStorage.longitude);
+   // localStorage.setItem('longitude', crd.longitude);
 }
 
-function AtualizaMapa() {
+function AtualizaMapa(latitude, longitude) {
 
-    var Long = sessionStorage.getItem('longitude');
-    var Lat = sessionStorage.getItem('latitude');;
-    var newLatLng = new L.LatLng(Lat, Long);
-    marker.setLatLng(newLatLng);
+    //alert("AtualizaMapa");
+    var map = L.map('map').setView([latitude, longitude], 13);
+    var marker = L.marker([latitude, longitude]).addTo(map);
+    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+        id: 'vitorcastro.cigoy6vp700eyusm1aljj8ifi',
+        accessToken: 'pk.eyJ1Ijoidml0b3JjYXN0cm8iLCJhIjoiY2lnb3k2d3ZoMDBmMnVzbTFqc3VhajMybSJ9._iZ9ySIFcj7si5opBAwDwA'
+    }).addTo(map);
 
-    marker.setLatLng(e.latlng);
-    map.setView(marker.getLatLng(), map.getZoom());
-    alert('Marker has been set to position :' + marker.getLatLng().toString());
+    sessionStorage.setItem('mapflag', '0');
+
+    return map;
+
 } 
 
-function IniciaMapa(latitude, longitude) {
+function CarregaMapa(latitude, longitude) {
+
+
+   // alert("CarregaMapa");
 
     var map = L.map('map').setView([latitude, longitude], 13);
-
    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
        maxZoom: 18,
@@ -32,31 +36,40 @@ function IniciaMapa(latitude, longitude) {
        accessToken: 'pk.eyJ1Ijoidml0b3JjYXN0cm8iLCJhIjoiY2lnb3k2d3ZoMDBmMnVzbTFqc3VhajMybSJ9._iZ9ySIFcj7si5opBAwDwA'
    }).addTo(map);
 
+   sessionStorage.setItem('mapflag', '1');
+
     return map;
 }
 
 
+document.addEventListener("deviceready", onDeviceReady, false);
+var optionsWatchPosition = { frequency: 3000, enableHighAccuracy: true };
+//var optionsGetCurrentPosition = { maximumAge: 3000, enableHighAccuracy: true };
 
-//document.addEventListener("deviceready", onDeviceReady, false);
-//var optionsWatchPosition = { frequency: 3000, enableHighAccuracy: true };
+function success(pos) {
 
-//function success(pos) {
-//    if (sessionStorage.getItem('longitude') == '' || sessionStorage.getItem('latitude') == '') {
-//        IniciaMapa(crd.latitude, crd.longitude);
-//    }
-//    else {
-//        AtualizaMapa();
-//    }
-//};
+    var crd = pos.coords;
 
-//function error(err) {
-//    alert('ERROR(' + err.code + '): ' + err.message);
-//};
+    if (sessionStorage.mapflag == null)
+    {
+        CarregaMapa(crd.latitude, crd.longitude);
+    }
+    else
+    {
+        AtualizaMapa(crd.latitude, crd.longitude)
+    }
 
-//function onDeviceReady() {
-//    navigator.geolocation.watchPosition(success, error, optionsWatchPosition);
-//}
-//navigator.geolocation.getCurrentPosition(success, error, optionsWatchPosition);
-//cordova.plugins.locationServices.geolocation.watchPosition(success, error, optionsWatchPosition);
-//alert('tentativa location services');
-//var optionsGetCurrentPosition = { maximumAge: 3000, enableHighAccuracy: true};
+};
+
+function error(err)
+{
+    alert('ERROR(' + err.code + '): ' + err.message);
+};
+
+function onDeviceReady()
+{
+    navigator.geolocation.watchPosition(success, error, optionsWatchPosition);
+    //navigator.geolocation.getCurrentPosition(success, error, optionsWatchPosition);
+}
+
+
