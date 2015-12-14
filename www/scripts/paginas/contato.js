26be1d38cd106eb5c3a30479562c370af6584e41
Vitor Castro <vitorcastro78@gmail.com>
2015-12-14 21:19:51 -0200

    sd

--- www/scripts/paginas/contato.js
+++ www/scripts/paginas/contato.js
@@ -12,28 +12,45 @@ function MontaTela() {
 
 function EnviaMensagem() {
 
-    BloqueiaTela("Enviando a sua mendagem...");
+  
+
+    BloqueiaTela("Enviando a sua mensagem...");
 
     var NOME = jQuery("#NOME").val();
     var EMAIL = jQuery("#EMAIL").val();
     var MENSAGEM = jQuery("#MENSAGEM").val();
     var ASSUNTO = jQuery("#ASSUNTO").val();
-    var HTML = "<div>" +
-    "<div>Nome : " + NOME+ " </div> "+
-    "<div>Email : " + EMAIL + "</div> "+
-    "<div>Assunto : " + ASSUNTO + " </div> "+
-    "<div><p>"+MENSAGEM+"</p></div></div>";
 
-    var resp = jQuery.parseJSON(EnviaEmail(HTML, ASSUNTO, null, ERROCONEXAO));
-    DesbloqueiaTela(2000);
+    if (ValidaMensagem(MENSAGEM, ASSUNTO)) {
+        var HTML = "<div>" +
+        "<div>Nome : " + NOME + " </div> " +
+        "<div>Email : " + EMAIL + "</div> " +
+        "<div>Assunto : " + ASSUNTO + " </div> " +
+        "<div><p>" + MENSAGEM + "</p></div></div>";
 
-   if(resp == 'OK')
-   {
-       ExibeMensagem("Mensagem enviada com sucesso");
-   }
-   else {
-       ExibeMensagem("Ocorreu um problema ao enviar sua mensagem, tente novamente");
-   }
+        var resp = jQuery.parseJSON(EnviaEmail(HTML, ASSUNTO, null, ERROCONEXAO));
+        DesbloqueiaTela(2000);
+
+        if (resp == 'OK') {
+            ExibeMensagem("Mensagem enviada com sucesso");
+        }
+        else {
+            ExibeMensagem("Ocorreu um problema ao enviar sua mensagem, tente novamente");
+        }
+    } else {
+
+        ExibeMensagem("Dados de assunto e mensagem são obrigatórios");
+    }
     
 }
 
+function ValidaMensagem(MENSAGEM,ASSUNTO) {
+
+    if (MENSAGEM == ''|| ASSUNTO == '') {
+        return false;
+    }
+    else {
+        return true;
+    }
+
+}
