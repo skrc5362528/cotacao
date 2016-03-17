29f62398d1c18f945f392dfe6f8d883fe38ffb3f
vitcastro <vitor.castro@topdown.com.br>
2016-03-17 17:30:11 -0300

    asdf

---
+++ platforms/android/assets/www/scripts/jsmodel/Bancos.js
@@ -0,0 +1,860 @@
+﻿var BANCOS = [
+      {
+          "CODIGO": "1",
+          "BANCO": " BANCO DO BRASIL S.A.",
+          
+      },
+    {
+        "CODIGO": "341",
+        "BANCO": "ITAÚ UNIBANCO S.A.",
+        
+    },
+     {
+         "CODIGO": "237",
+         "BANCO": "BANCO BRADESCO S.A.",
+         
+     },
+      {
+          "CODIGO": "33 ",
+          "BANCO": "BANCO SANTANDER (BRASIL) S.A.",
+          
+      },
+        {
+            "CODIGO": "104",
+            "BANCO": "CAIXA ECONÔMICA FEDERAL",
+            
+        },
+
+          {
+              "CODIGO": "745",
+              "BANCO": "BANCO CITIBANK S.A.",
+              
+          },
+            {
+                "CODIGO": "399",
+                "BANCO": "HSBC BANK BRASIL S.A. - BANCO MÚLTIPLO",
+                
+            }
+];
+var TODOS_BANCOS = [{
+      "CODIGO": "477",
+      "BANCO": "CITIBANK N.A.",
+      
+  },
+  {
+      "CODIGO": "654",
+      "BANCO": "BANCO A.J.RENNER S.A.",
+      
+  },
+  {
+      "CODIGO": "246",
+      "BANCO": "BANCO ABC BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "25 ",
+      "BANCO": "BANCO ALFA S.A.",
+      
+  },
+  {
+      "CODIGO": "641",
+      "BANCO": "BANCO ALVORADA S.A.",
+      
+  },
+  {
+      "CODIGO": "213",
+      "BANCO": "BANCO ARBI S.A.",
+      
+  },
+  {
+      "CODIGO": "19 ",
+      "BANCO": "BANCO AZTECA DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "29 ",
+      "BANCO": "BANCO BANERJ S.A.",
+      
+  },
+  {
+      "CODIGO": "0",
+      "BANCO": " BANCO BANKPAR S.A.",
+      
+  },
+  {
+      "CODIGO": "740",
+      "BANCO": "BANCO BARCLAYS S.A.",
+      
+  },
+  {
+      "CODIGO": "107",
+      "BANCO": "BANCO BBM S.A.",
+      
+  },
+  {
+      "CODIGO": "31 ",
+      "BANCO": "BANCO BEG S.A.",
+      
+  },
+  {
+      "CODIGO": "739",
+      "BANCO": "BANCO BGN S.A.",
+      
+  },
+  {
+      "CODIGO": "96 ",
+      "BANCO": "BANCO BM&F DE SERVIÇOS DE LIQUIDAÇÃO E CUSTÓDIA S.A",
+      
+  },
+  {
+      "CODIGO": "318",
+      "BANCO": "BANCO BMG S.A.",
+      
+  },
+  {
+      "CODIGO": "752",
+      "BANCO": "BANCO BNP PARIBAS BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "248",
+      "BANCO": "BANCO BOAVISTA INTERATLÂNTICO S.A.",
+      
+  },
+  {
+      "CODIGO": "218",
+      "BANCO": "BANCO BONSUCESSO S.A.",
+      
+  },
+  {
+      "CODIGO": "65 ",
+      "BANCO": "BANCO BRACCE S.A.",
+      
+  },
+  {
+      "CODIGO": "36 ",
+      "BANCO": "BANCO BRADESCO BBI S.A.",
+      
+  },
+  {
+      "CODIGO": "204",
+      "BANCO": "BANCO BRADESCO CARTÕES S.A.",
+      
+  },
+  {
+      "CODIGO": "394",
+      "BANCO": "BANCO BRADESCO FINANCIAMENTOS S.A.",
+      
+  },
+
+  {
+      "CODIGO": "225",
+      "BANCO": "BANCO BRASCAN S.A.",
+      
+  },
+  {
+      "CODIGO": "M15",
+      "BANCO": "BANCO BRJ S.A.",
+      
+  },
+  {
+      "CODIGO": "208",
+      "BANCO": "BANCO BTG PACTUAL S.A.",
+      
+  },
+  {
+      "CODIGO": "44 ",
+      "BANCO": "BANCO BVA S.A.",
+      
+  },
+  {
+      "CODIGO": "263",
+      "BANCO": "BANCO CACIQUE S.A.",
+      
+  },
+  {
+      "CODIGO": "473",
+      "BANCO": "BANCO CAIXA GERAL - BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "412",
+      "BANCO": "BANCO CAPITAL S.A.",
+      
+  },
+  {
+      "CODIGO": "40 ",
+      "BANCO": "BANCO CARGILL S.A.",
+      
+  },
+
+  {
+      "CODIGO": "M08",
+      "BANCO": "BANCO CITICARD S.A.",
+      
+  },
+  {
+      "CODIGO": "241",
+      "BANCO": "BANCO CLÁSSICO S.A.",
+      
+  },
+  {
+      "CODIGO": "M19",
+      "BANCO": "BANCO CNH CAPITAL S.A.",
+      
+  },
+  {
+      "CODIGO": "215",
+      "BANCO": "BANCO COMERCIAL E DE INVESTIMENTO SUDAMERIS S.A.",
+      
+  },
+  {
+      "CODIGO": "756",
+      "BANCO": "BANCO COOPERATIVO DO BRASIL S.A. - BANCOOB",
+      
+  },
+  {
+      "CODIGO": "748",
+      "BANCO": "BANCO COOPERATIVO SICREDI S.A.",
+      
+  },
+  {
+      "CODIGO": "75 ",
+      "BANCO": "BANCO CR2 S.A.",
+      
+  },
+  {
+      "CODIGO": "721",
+      "BANCO": "BANCO CREDIBEL S.A.",
+      
+  },
+  {
+      "CODIGO": "222",
+      "BANCO": "BANCO CREDIT AGRICOLE BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "505",
+      "BANCO": "BANCO CREDIT SUISSE (BRASIL) S.A.",
+      
+  },
+  {
+      "CODIGO": "229",
+      "BANCO": "BANCO CRUZEIRO DO SUL S.A.",
+      
+  },
+  {
+      "CODIGO": "266",
+      "BANCO": "BANCO CÉDULA S.A.",
+      
+  },
+  {
+      "CODIGO": "3",
+      "BANCO": " BANCO DA AMAZÔNIA S.A.",
+      
+  },
+  {
+      "CODIGO": "083-3 ",
+      "BANCO": "BANCO DA CHINA BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "M21",
+      "BANCO": "BANCO DAIMLERCHRYSLER S.A.",
+      
+  },
+  {
+      "CODIGO": "707",
+      "BANCO": "BANCO DAYCOVAL S.A.",
+      
+  },
+  {
+      "CODIGO": "300",
+      "BANCO": "BANCO DE LA NACION ARGENTINA",
+      
+  },
+  {
+      "CODIGO": "495",
+      "BANCO": "BANCO DE LA PROVINCIA DE BUENOS AIRES",
+      
+  },
+  {
+      "CODIGO": "494",
+      "BANCO": "BANCO DE LA REPUBLICA ORIENTAL DEL URUGUAY",
+      
+  },
+  {
+      "CODIGO": "M06",
+      "BANCO": "BANCO DE LAGE LANDEN BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "24 ",
+      "BANCO": "BANCO DE PERNAMBUCO S.A. - BANDEPE",
+      
+  },
+  {
+      "CODIGO": "456",
+      "BANCO": "BANCO DE TOKYO-MITSUBISHI UFJ BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "214",
+      "BANCO": "BANCO DIBENS S.A.",
+      
+  },
+
+  {
+      "CODIGO": "47 ",
+      "BANCO": "BANCO DO ESTADO DE SERGIPE S.A.",
+      
+  },
+  {
+      "CODIGO": "37 ",
+      "BANCO": "BANCO DO ESTADO DO PARÁ S.A.",
+      
+  },
+  {
+      "CODIGO": "39 ",
+      "BANCO": "BANCO DO ESTADO DO PIAUÍ S.A. - BEP",
+      
+  },
+  {
+      "CODIGO": "41 ",
+      "BANCO": "BANCO DO ESTADO DO RIO GRANDE DO SUL S.A.",
+      
+  },
+  {
+      "CODIGO": "4",
+      "BANCO": " BANCO DO NORDESTE DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "265",
+      "BANCO": "BANCO FATOR S.A.",
+      
+  },
+  {
+      "CODIGO": "M03",
+      "BANCO": "BANCO FIAT S.A.",
+      
+  },
+  {
+      "CODIGO": "224",
+      "BANCO": "BANCO FIBRA S.A.",
+      
+  },
+  {
+      "CODIGO": "626",
+      "BANCO": "BANCO FICSA S.A.",
+      
+  },
+  {
+      "CODIGO": "M18",
+      "BANCO": "BANCO FORD S.A.",
+      
+  },
+  {
+      "CODIGO": "233",
+      "BANCO": "BANCO GE CAPITAL S.A.",
+      
+  },
+  {
+      "CODIGO": "734",
+      "BANCO": "BANCO GERDAU S.A.",
+      
+  },
+  {
+      "CODIGO": "M07",
+      "BANCO": "BANCO GMAC S.A.",
+      
+  },
+  {
+      "CODIGO": "612",
+      "BANCO": "BANCO GUANABARA S.A.",
+      
+  },
+  {
+      "CODIGO": "M22",
+      "BANCO": "BANCO HONDA S.A.",
+      
+  },
+  {
+      "CODIGO": "63 ",
+      "BANCO": "BANCO IBI S.A. BANCO MÚLTIPLO",
+      
+  },
+  {
+      "CODIGO": "M11",
+      "BANCO": "BANCO IBM S.A.",
+      
+  },
+  {
+      "CODIGO": "604",
+      "BANCO": "BANCO INDUSTRIAL DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "320",
+      "BANCO": "BANCO INDUSTRIAL E COMERCIAL S.A.",
+      
+  },
+  {
+      "CODIGO": "653",
+      "BANCO": "BANCO INDUSVAL S.A.",
+      
+  },
+  {
+      "CODIGO": "630",
+      "BANCO": "BANCO INTERCAP S.A.",
+      
+  },
+  {
+      "CODIGO": "077-9 ",
+      "BANCO": "BANCO INTERMEDIUM S.A.",
+      
+  },
+  {
+      "CODIGO": "249",
+      "BANCO": "BANCO INVESTCRED UNIBANCO S.A.",
+      
+  },
+  {
+      "CODIGO": "M09",
+      "BANCO": "BANCO ITAUCRED FINANCIAMENTOS S.A.",
+      
+  },
+  {
+      "CODIGO": "184",
+      "BANCO": "BANCO ITAÚ BBA S.A.",
+      
+  },
+  {
+      "CODIGO": "479",
+      "BANCO": "BANCO ITAÚBANK S.A",
+      
+  },
+  {
+      "CODIGO": "376",
+      "BANCO": "BANCO J. P. MORGAN S.A.",
+      
+  },
+  {
+      "CODIGO": "74 ",
+      "BANCO": "BANCO J. SAFRA S.A.",
+      
+  },
+  {
+      "CODIGO": "217",
+      "BANCO": "BANCO JOHN DEERE S.A.",
+      
+  },
+  {
+      "CODIGO": "76 ",
+      "BANCO": "BANCO KDB S.A.",
+      
+  },
+  {
+      "CODIGO": "757",
+      "BANCO": "BANCO KEB DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "600",
+      "BANCO": "BANCO LUSO BRASILEIRO S.A.",
+      
+  },
+  {
+      "CODIGO": "212",
+      "BANCO": "BANCO MATONE S.A.",
+      
+  },
+  {
+      "CODIGO": "M12",
+      "BANCO": "BANCO MAXINVEST S.A.",
+      
+  },
+  {
+      "CODIGO": "389",
+      "BANCO": "BANCO MERCANTIL DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "746",
+      "BANCO": "BANCO MODAL S.A.",
+      
+  },
+  {
+      "CODIGO": "M10",
+      "BANCO": "BANCO MONEO S.A.",
+      
+  },
+  {
+      "CODIGO": "738",
+      "BANCO": "BANCO MORADA S.A.",
+      
+  },
+  {
+      "CODIGO": "66 ",
+      "BANCO": "BANCO MORGAN STANLEY S.A.",
+      
+  },
+  {
+      "CODIGO": "243",
+      "BANCO": "BANCO MÁXIMA S.A.",
+      
+  },
+  {
+      "CODIGO": "45 ",
+      "BANCO": "BANCO OPPORTUNITY S.A.",
+      
+  },
+  {
+      "CODIGO": "M17",
+      "BANCO": "BANCO OURINVEST S.A.",
+      
+  },
+  {
+      "CODIGO": "623",
+      "BANCO": "BANCO PANAMERICANO S.A.",
+      
+  },
+  {
+      "CODIGO": "611",
+      "BANCO": "BANCO PAULISTA S.A.",
+      
+  },
+  {
+      "CODIGO": "613",
+      "BANCO": "BANCO PECÚNIA S.A.",
+      
+  },
+  {
+      "CODIGO": "094-2 ",
+      "BANCO": "BANCO PETRA S.A.",
+      
+  },
+  {
+      "CODIGO": "643",
+      "BANCO": "BANCO PINE S.A.",
+      
+  },
+  {
+      "CODIGO": "724",
+      "BANCO": "BANCO PORTO SEGURO S.A.",
+      
+  },
+  {
+      "CODIGO": "735",
+      "BANCO": "BANCO POTTENCIAL S.A.",
+      
+  },
+  {
+      "CODIGO": "638",
+      "BANCO": "BANCO PROSPER S.A.",
+      
+  },
+  {
+      "CODIGO": "M24",
+      "BANCO": "BANCO PSA FINANCE BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "747",
+      "BANCO": "BANCO RABOBANK INTERNATIONAL BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "088-4 ",
+      "BANCO": "BANCO RANDON S.A.",
+      
+  },
+  {
+      "CODIGO": "356",
+      "BANCO": "BANCO REAL S.A.",
+      
+  },
+  {
+      "CODIGO": "633",
+      "BANCO": "BANCO RENDIMENTO S.A.",
+      
+  },
+  {
+      "CODIGO": "741",
+      "BANCO": "BANCO RIBEIRÃO PRETO S.A.",
+      
+  },
+  {
+      "CODIGO": "M16",
+      "BANCO": "BANCO RODOBENS S.A.",
+      
+  },
+  {
+      "CODIGO": "72 ",
+      "BANCO": "BANCO RURAL MAIS S.A.",
+      
+  },
+  {
+      "CODIGO": "453",
+      "BANCO": "BANCO RURAL S.A.",
+      
+  },
+  {
+      "CODIGO": "422",
+      "BANCO": "BANCO SAFRA S.A.",
+      
+  },
+  
+  {
+      "CODIGO": "250",
+      "BANCO": "BANCO SCHAHIN S.A.",
+      
+  },
+  {
+      "CODIGO": "743",
+      "BANCO": "BANCO SEMEAR S.A.",
+      
+  },
+  {
+      "CODIGO": "749",
+      "BANCO": "BANCO SIMPLES S.A.",
+      
+  },
+  {
+      "CODIGO": "366",
+      "BANCO": "BANCO SOCIÉTÉ GÉNÉRALE BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "637",
+      "BANCO": "BANCO SOFISA S.A.",
+      
+  },
+  {
+      "CODIGO": "12 ",
+      "BANCO": "BANCO STANDARD DE INVESTIMENTOS S.A.",
+      
+  },
+  {
+      "CODIGO": "464",
+      "BANCO": "BANCO SUMITOMO MITSUI BRASILEIRO S.A.",
+      
+  },
+  {
+      "CODIGO": "082-5 ",
+      "BANCO": "BANCO TOPÁZIO S.A.",
+      
+  },
+  {
+      "CODIGO": "M20",
+      "BANCO": "BANCO TOYOTA DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "M13",
+      "BANCO": "BANCO TRICURY S.A.",
+      
+  },
+  {
+      "CODIGO": "634",
+      "BANCO": "BANCO TRIÂNGULO S.A.",
+      
+  },
+  {
+      "CODIGO": "M14",
+      "BANCO": "BANCO VOLKSWAGEN S.A.",
+      
+  },
+  {
+      "CODIGO": "M23",
+      "BANCO": "BANCO VOLVO (BRASIL) S.A.",
+      
+  },
+  {
+      "CODIGO": "655",
+      "BANCO": "BANCO VOTORANTIM S.A.",
+      
+  },
+  {
+      "CODIGO": "610",
+      "BANCO": "BANCO VR S.A.",
+      
+  },
+  {
+      "CODIGO": "370",
+      "BANCO": "BANCO WESTLB DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "21 ",
+      "BANCO": "BANESTES S.A. BANCO DO ESTADO DO ESPÍRITO SANTO",
+      
+  },
+  {
+      "CODIGO": "719",
+      "BANCO": " BANIF-BANCO INTERNACIONAL DO FUNCHAL (BRASIL)S.A.",
+      
+  },
+  {
+      "CODIGO": "755",
+      "BANCO": "BANK OF AMERICA MERRILL LYNCH BANCO MÚLTIPLO S.A.",
+      
+  },
+  {
+      "CODIGO": "744",
+      "BANCO": "BANKBOSTON N.A.",
+      
+  },
+  {
+      "CODIGO": "73 ",
+      "BANCO": "BB BANCO POPULAR DO BRASIL S.A.",
+      
+  },
+  {
+      "CODIGO": "78 ",
+      "BANCO": "BES INVESTIMENTO DO BRASIL S.A.-BANCO DE INVESTIMENTO",
+      
+  },
+  {
+      "CODIGO": "69 ",
+      "BANCO": "BPN BRASIL BANCO MÚLTIPLO S.A.",
+      
+  },
+  {
+      "CODIGO": "70 ",
+      "BANCO": "BRB - BANCO DE BRASÍLIA S.A.",
+      
+  },
+  {
+      "CODIGO": "092-2 ",
+      "BANCO": "BRICKELL S.A. CRÉDITO",
+      "FIELD3": " FINANCIAMENTO E INVESTIMENTO"
+  },
+
+  {
+      "CODIGO": "081-7 ",
+      "BANCO": "CONCÓRDIA BANCO S.A.",
+      
+  },
+  {
+      "CODIGO": "097-3 ",
+      "BANCO": "COOPERATIVA CENTRAL DE CRÉDITO NOROESTE BRASILEIRO LTDA.",
+      
+  },
+  {
+      "CODIGO": "085-X ",
+      "BANCO": "COOPERATIVA CENTRAL DE CRÉDITO URBANO-CECRED",
+      
+  },
+  {
+      "CODIGO": "099-X ",
+      "BANCO": "COOPERATIVA CENTRAL DE ECONOMIA E CREDITO MUTUO DAS UNICREDS",
+      
+  },
+  {
+      "CODIGO": "090-2 ",
+      "BANCO": "COOPERATIVA CENTRAL DE ECONOMIA E CRÉDITO MUTUO DAS UNICREDS",
+      
+  },
+  {
+      "CODIGO": "089-2 ",
+      "BANCO": "COOPERATIVA DE CRÉDITO RURAL DA REGIÃO DE MOGIANA",
+      
+  },
+  {
+      "CODIGO": "087-6 ",
+      "BANCO": "COOPERATIVA UNICRED CENTRAL SANTA CATARINA",
+      
+  },
+  {
+      "CODIGO": "098-1 ",
+      "BANCO": "CREDICOROL COOPERATIVA DE CRÉDITO RURAL",
+      
+  },
+  {
+      "CODIGO": "487",
+      "BANCO": "DEUTSCHE BANK S.A. - BANCO ALEMÃO",
+      
+  },
+  {
+      "CODIGO": "751",
+      "BANCO": "DRESDNER BANK BRASIL S.A. - BANCO MÚLTIPLO",
+      
+  },
+  {
+      "CODIGO": "64 ",
+      "BANCO": "GOLDMAN SACHS DO BRASIL BANCO MÚLTIPLO S.A.",
+      
+  },
+  {
+      "CODIGO": "62 ",
+      "BANCO": "HIPERCARD BANCO MÚLTIPLO S.A.",
+      
+  },
+  {
+      "CODIGO": "168",
+      "BANCO": "HSBC FINANCE (BRASIL) S.A. - BANCO MÚLTIPLO",
+      
+  },
+  {
+      "CODIGO": "492",
+      "BANCO": "ING BANK N.V.",
+      
+  },
+  {
+      "CODIGO": "652",
+      "BANCO": "ITAÚ UNIBANCO HOLDING S.A.",
+      
+  },
+
+  {
+      "CODIGO": "79 ",
+      "BANCO": "JBS BANCO S.A.",
+      
+  },
+  {
+      "CODIGO": "488",
+      "BANCO": "JPMORGAN CHASE BANK",
+      
+  },
+  {
+      "CODIGO": "14 ",
+      "BANCO": "NATIXIS BRASIL S.A. BANCO MÚLTIPLO",
+      
+  },
+  {
+      "CODIGO": "753",
+      "BANCO": "NBC BANK BRASIL S.A. - BANCO MÚLTIPLO",
+      
+  },
+  {
+      "CODIGO": "086-8 ",
+      "BANCO": "OBOE CRÉDITO FINANCIAMENTO E INVESTIMENTO S.A.",
+      
+  },
+  {
+      "CODIGO": "254",
+      "BANCO": "PARANÁ BANCO S.A.",
+      
+  },
+  {
+      "CODIGO": "409",
+      "BANCO": "UNIBANCO - UNIÃO DE BANCOS BRASILEIROS S.A.",
+      
+  },
+  {
+      "CODIGO": "230",
+      "BANCO": "UNICARD BANCO MÚLTIPLO S.A.",
+      
+  },
+  {
+      "CODIGO": "091-4 ",
+      "BANCO": "UNICRED CENTRAL DO RIO GRANDE DO SUL",
+      
+  },
+  {
+      "CODIGO": "84",
+      "BANCO": "UNICRED NORTE DO PARANÁ",
+      
+  }
+];
