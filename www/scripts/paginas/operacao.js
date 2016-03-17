f553604c43501a91d31fb191d5a48207902932f6
Vitor Castro <vitorcastro78@gmail.com>
2016-03-17 11:35:34 -0300

    ddd

--- www/scripts/paginas/operacao.js
+++ www/scripts/paginas/operacao.js
@@ -13,15 +13,15 @@ var IMAGEM_BASE64_CPF = '';
 var IMAGEM_BASE64_RG = '';
 var IMAGEM_BASE64_COMPROVANTE = '';
 var IOF = jQuery.parseJSON(localStorage.getItem('SYSCONFIG'))[0].IOF;
-var pictureSource = navigator.camera.PictureSourceType;
-var destinationType = navigator.camera.DestinationType; // sets the format of returned value
+var pictureSource;
+var destinationType; // sets the format of returned value
 
-//document.addEventListener("deviceready", onDeviceReady, false);
+document.addEventListener("deviceready", onDeviceReady, false);
 
-//function onDeviceReady() {
-//    pictureSource 
-//    destinationType 
-//}
+function onDeviceReady() {
+    pictureSource = navigator.camera.PictureSourceType;
+    destinationType = navigator.camera.DestinationType; 
+}
 
 function BuscaCotacaoEstabelecimento() {
     DATA_COTACAO = jQuery.parseJSON(RetornaCotacaoEstabelecimentoPorMoeda(ID_ESTABELECIMENTO, SIMBOLO, null, null));
