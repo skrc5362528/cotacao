0fb5bdd703b29d0f17b36d23a8f2f1b4ffe20a0b
vitorcastro78@gmail.com <lara2308>
2016-04-10 11:20:54 -0300

    asdf

---
+++ platforms/android/assets/www/scripts/paginas/estabelecimentomoeda.js
@@ -0,0 +1,198 @@
+﻿function CarregaSelectEstabelecimentos() {
+    jQuery('#ESTABELECIMENTO_').append('<option value="" selected> Que estabelecimento você procura? </option>');
+    var data = jQuery.parseJSON(RetornaListaEstabelecimentos(null, ERROCONEXAO));
+    if (data.length > 0) {
+        jQuery.each(data, function () {
+            MontaSelectEstabelecimento('ESTABELECIMENTO_', this.ID_ESTABELECIMENTO, this.NOME);
+        });
+    }
+}
+
+function PreencheSelectMoeda() {
+    jQuery('#MOEDA').append('<option value="" selected> Que moeda você procura? </option>');
+    var data = jQuery.parseJSON(ListaMoeda(null, ERROCONEXAO));
+    if (data.length > 0) {
+        jQuery.each(data, function () {
+            MontaSelect('MOEDA', this.SIMBOLO, this.NOME, this.PAIS);
+        });
+    }
+}
+
+function MontaSelect(OBJETO, SIMBOLO, NOME, PAIS) {
+    jQuery('#' + OBJETO + '').append('<option value=' + SIMBOLO + '>' + NOME + '</option>');
+}
+
+function MontaSelectEstabelecimento(OBJETO, CODIGO, NOME) {
+
+    jQuery('#' + OBJETO + '').append('<option value=' + CODIGO + '>' + NOME + ' </option>');
+}
+
+function CarregaEstabelecimento(data) {
+    var html =
+    "<div id='" + data.ID_ESTABELECIMENTO + "' class='big-notification static-notification-white'>" +
+    "<strong><label class='busca-text'>" + data.NOME + "</label></strong> " +
+    "<div class='one-half'>" +
+    "<label class='contact-text'> " + data.FONE + "</label>" +
+    "<label class='contact-text'>R$: 10,00 </label>" +// + + data.VALOR_COTACAO + "</label>" +
+    "</div>" +
+    "<div class='two-half last-column'>" +
+    "<label class='contact-text'></label>" +
+    "</div>" +
+     "<div>" +
+    "</div>" +
+    "</div>" +
+    "</br>";
+
+    return html;
+}
+
+function RetornaEstabelecimento(ID_ESTABELECIMENTO) {
+    var data = jQuery.parseJSON(RetornaEstabelecimentoPorId(ID_ESTABELECIMENTO, null, ERROCONEXAO));
+    if (data.length > 0) {
+        jQuery.each(data, function () {
+            CarregaEstabelecimento(data);
+        });
+    }
+}
+
+function AdicionaMoeda(ID_ESTABELECIMENTO) {
+    var ID_MOEDA = '';
+    var SIMBOLO = '';
+    var NOME = '';
+    var CODIGO = '';
+    var MOEDA = jQuery.parseJSON(ListaMoeda(null, ERROCONEXAO));
+    jQuery.each(MOEDA, function () {
+        if (this.SIMBOLO == jQuery('#MOEDA').val()) {
+            ID_MOEDA = this.ID_MOEDA;
+            SIMBOLO = this.SIMBOLO;
+            NOME = this.NOME;
+            CODIGO = this.COD_MOEDA;
+        }
+    });
+    var VALOR_COTACAO = jQuery('#VALOR_COTACAO').val();
+    var VALOR_COTACAO_COMPRA = jQuery('#VALOR_COTACAO_COMPRA').val();
+
+
+    var dt = new Date();
+    var data = jQuery.parseJSON(RetornaCotacaoEstabelecimentoPorMoeda(ID_ESTABELECIMENTO, SIMBOLO, null, ERROCONEXAO));
+
+    if (data.length == 0) {
+
+        InsereMoedaEstabelecimento(ID_ESTABELECIMENTO, ID_MOEDA, CODIGO, NOME, SIMBOLO, null, ERROCONEXAO)
+        InsereCotacaoEstabelecimento(ID_ESTABELECIMENTO, VALOR_COTACAO, VALOR_COTACAO_COMPRA, ID_MOEDA, CODIGO, SIMBOLO, dt.getDate(), (dt.getMonth() + 1), dt.getFullYear(), null, null);
+    }
+    else {
+        ExibeMensagem('Moeda já cadastrada!');
+    }
+    PreencheMoedasTrabalhadas(ID_ESTABELECIMENTO);
+}
+
+function CarregaMoedaTrabalhada(ID_ESTABELECIMENTO, SIMBOLO) {
+    var data = jQuery.parseJSON(RetornaCotacaoEstabelecimentoPorMoeda(ID_ESTABELECIMENTO, SIMBOLO, null, ERROCONEXAO));
+    if (data.length > 0) {
+        jQuery.each(data, function () {
+            jQuery('#VALOR_COTACAO').val(this.VALOR_COTACAO);
+            jQuery('#VALOR_COTACAO_COMPRA').val(this.VALOR_COTACAO_COMPRA);
+            jQuery('#STATUS').val(this.STATUS);
+        });
+    }
+}
+
+function PreencheMoedasTrabalhadas(ID_ESTABELECIMENTO) {
+    var html = '';
+    var data = jQuery.parseJSON(RetornaListaMoedaEstabelecimentoCotacao(ID_ESTABELECIMENTO, null, ERROCONEXAO));
+    if (data.length > 0) {
+        jQuery.each(data, function () {
+            html += MontaMoedasTrabalhadas(this);
+        });
+    }
+    jQuery('#DIVESTABELECIMENTO').html(html);
+}
+
+function MontaMoedasTrabalhadas(data) {
+
+    var status = verificaAtivo(data.ATIVO);
+
+    var html =
+   "<div id='" + data.ID_COTACAO + "' class='big-notification static-notification-white'>" +
+    "<div>" +
+    "<strong><label class='contact-text'>" + data.NOME_MOEDA + "</label></strong> " +
+    "</div>" +
+    "<div class='one-third center-text'>" +
+     "<a  onclick='AlteraCotacao(this);' id='ALTERA_" + data.ID_COTACAO + "_" + data.ID_MOEDA + "'   class='button button-white'><i class='fa fa-pencil' style='font-size:18px; color:#0489B1;'></i></a>" +
+   "</div>" +
+   "<div class='one-third center-text'>" +
+     "<a  onclick='AlteraCotacao(this);' id='CANCELA_" + data.ID_COTACAO + "_" + data.ID_MOEDA + "'  class='button button-white'><i class='fa fa-times' style='font-size:18px; color:#0489B1;'></i></a>" +
+  "</div>" +
+   "<div class='one-third last-column center-text'>" +
+   "<a  onclick='AlteraCotacao(this);' id='GRAVA_" + data.ID_COTACAO + "_" + data.ID_MOEDA + "'  class='button button-white'><i class='fa fa-floppy-o' style='font-size:18px; color:#0489B1;'></i></a>" +
+   "</div>" +
+   "<div class='one-half'>" +
+   "<fieldset>" +
+   "<label class='contact-text'> Cod.:" + data.SIMBOLO + "</label>" +
+   "<label class='contact-text'>Perc. Venda:</label>" +
+    "<input type='number' name='VALOR_COTACAO' value='" + data.VALOR_COTACAO + "' class='contactField' id='VALOR_COTACAO_" + data.ID_COTACAO + "' placeholder='Percentual de valoração' required readonly />" +
+    "<fieldset>" +
+    "</div>" +
+    "<div class='one-half last-column'>" +
+    "<fieldset>" +
+    "<label class='contact-text'>Status:" + status + "</label>" +
+    "<label class='contact-text'>Perc. Compra: </label>" +
+    "<input type='number' name='VALOR_COTACAO_COMPRA' value='" + data.VALOR_COTACAO_COMPRA + "' class='contactField' id='VALOR_COTACAO_COMPRA_" + data.ID_COTACAO + "' placeholder='Percentual de valoração' required readonly />" +
+    "<fieldset>" +
+    "</div>" +
+    "</div>";
+
+
+    return html;
+
+
+}
+
+jQuery(document).ready(function () {
+    CarregaSelectEstabelecimentos();
+    PreencheSelectMoeda();
+    EqualizaTamanhoTela();
+});
+
+function verificaAtivo(ATIVO) {
+    var ret = '';
+    if (ATIVO == true)
+        ret = 'ATIVO';
+    else
+        ret = 'DESATIVADO';
+
+    return ret;
+}
+
+function AlteraCotacao(obj) {
+    var ob = obj.id.split("_");
+    var ACAO = ob[0];
+    var ID_COTACAO = ob[1];
+    var ID_MOEDA = ob[2];
+
+
+    if (ACAO == "ALTERA")
+    {
+        jQuery('#VALOR_COTACAO_' + ID_COTACAO + '').attr("readonly", false);
+        jQuery('#VALOR_COTACAO_COMPRA_' + ID_COTACAO + '').attr("readonly", false);
+    }
+    if (ACAO == "CANCELA")
+    {
+        jQuery('#VALOR_COTACAO_' + ID_COTACAO + '').attr("readonly", true);
+        jQuery('#VALOR_COTACAO_COMPRA_' + ID_COTACAO + '').attr("readonly", true);
+    }
+    if (ACAO == "GRAVA")
+    {
+        var dt = new Date();
+        var VALOR_COTACAO = jQuery('#VALOR_COTACAO_' + ID_COTACAO + '').val();
+        var VALOR_COTACAO_COMPRA = jQuery('#VALOR_COTACAO_COMPRA_' + ID_COTACAO + '').val();
+        var ID_ESTABELECIMENTO = jQuery('#ESTABELECIMENTO_').val();
+        var ID_MOEDA = ID_MOEDA;
+        var DIA= dt.getDate();//, ,,
+        var MES =(dt.getMonth() + 1);
+        var ANO =  dt.getFullYear();
+
+        AlteraCotacaoEstabelecimento(ID_COTACAO, VALOR_COTACAO, VALOR_COTACAO_COMPRA, ID_ESTABELECIMENTO, ID_MOEDA, DIA, MES, ANO, null, ERROCONEXAO)
+    }
+}
---
+++ www/scripts/paginas/ranking.js
@@ -0,0 +1,166 @@
+﻿var OPERACAO = '';
+var COD_VENDA = '';
+var OP_REALIZADA = '';
+
+
+jQuery(document).ready(function myfunction() {
+    EqualizaTamanhoTela();
+    debugger
+    MontaTela();
+
+});
+
+function MontaTela() {
+    //   var usu = jQuery.parseJSON(localStorage.getItem("USUARIO"));
+    COD_VENDA = RecebeValores();
+    BuscaOperacaoUsuario(COD_VENDA);
+    CarregaDivConfirmacao(OPERACAO);
+}
+
+function CarregaDivConfirmacao(OPERACAO) {
+    debugger;
+    var html = "<div class='container no-bottom'>" +
+        "<div class='no-bottom'>   " +
+        "<div>  " +
+        "<fieldset>" +
+         " <div class='big-notification static-notification-white'> " +
+        "<h4 style='color:black;'><strong><label>Estabelecimento : " + OPERACAO[0].CONTA + "</label></strong>  " +
+        " <strong><label>Cod. Venda : " + OPERACAO[0].COD_VENDA + "</label></strong>  " +
+        " <strong><label>Moeda : " + OPERACAO[0].SIMBOLO + "</label></strong>  " +
+        " <strong><label>Valor Total : " +formataValores(parseFloat(OPERACAO[0].VALOR_TOTAL_OPERACAO).toFixed(2),"R$") + "</label></strong>   " +
+        " <strong><label>Estabelecimento : " + OPERACAO[0].CONTA + "</label></strong> </h4>  " +
+
+
+       // CarregaEstabelecimento(OPERACAO[0]) +
+
+        "<h2 style='color:black;'><p class='center-text' style='font-size:22px;'><strong> Qualifique sua compra </strong></p></h2>" +
+        "<p class='center-text' ><strong><i class='fa fa-check' style='font-size: 128px; color:#0489B1;'></i></strong></p>" +
+
+        "</div>" +
+        "</fieldset>" +
+        "</div>" +
+        "</div>" +
+        "</div>";
+    jQuery('#DIVDADOSRANKING').html(html);
+}
+
+function CarregaEstabelecimento(data) {
+
+    var dt = new Date(data.DATA_COTACAO);
+    var html =
+ "<div id='" + data.ID_ESTABELECIMENTO + "' class='big-notification static-notification-white' style='height: 140px;'>" +
+    "<div><img src='img/icon.png' style='float:left; width:60px;margin-right: 5px;'" +
+    "</div>" +
+    "<div class='two-half'>" +
+    "<strong>" + data.NOME + "</strong> " +
+    "</div>" +
+    "<div>" +
+    "<div class='one-half' style='width:95%;'>" +
+    "<strong>Venda:</strong> R$ " + data.VALOR_COTACAO +
+    "<strong>Dist.:</strong> " + data.DISTANCIA + " KM </div>" +
+    "</div>" +
+    "<div>" +
+   "<div class='one-half' style='width:95%;'>" +
+   "<strong>Atualização:</strong>" +
+   "</div>" +
+   "<div class='two-half' style='width:95%;margin-left: 70px;top: -20px;'>" +
+    dt.getDate() + "/" + (dt.getMonth() + 1) + "/" + dt.getFullYear() + " ás " + data.HORA_COTACAO +
+    "</div>" +
+    "</div>" +
+     "<div class='two-half last-column' style='margin-left: 1px;top: -19px;'>" +
+    "<span class='span-stars'>" +
+    "<i class='fa fa-star'></i>" +
+    "<i class='fa fa-star'></i>" +
+    "<i class='fa fa-star'></i>" +
+    "<i class='fa fa-star'></i>" +
+    "<i class='fa fa-star-o'></i>" +
+    "</span>" +
+    "</div>" +
+    "<div style='width:95%;margin-left: 189px;top: -40px;'>" +
+    "<strong>IOF 0,38% incluso </strong>    " +
+    "</div>" +
+
+
+
+    "<div class='static-notification-exchange' style='border-radius: 10px;top: -40px;' onclick='MostraReserva(this)' id='" + data.ID_ESTABELECIMENTO + "_" + data.SIMBOLO + "' >" +
+    "<p class='center-text' style='font-size:15px; color:white;'>Comprar</p>" +
+    "</div>" +
+
+
+   "<div style='border-radius: 10px;top: -40px;'>" +
+    MontaInfo(data.RETIRADA, data.DELIVERY, data.RECARGA);//"<label class='contact-text'>" + data.NOME + "</label>" +
+    + "</div>"
+    "</div>";
+    return html;
+
+}
+function RecebeValores() {
+    return localStorage.getItem('VIEWCODVENDA');
+}
+
+function 
+    BuscaOperacaoUsuario(COD_VENDA) {
+    OPERACAO = jQuery.parseJSON(RetornaOperacaoPorCodigo(COD_VENDA, null, null));
+    CarregaDivConfirmacao(OPERACAO);
+}
+
+function CarregaOperacao(OPERACAO) {
+    jQuery('#NOME').val(OPERACAO[0].NOME);
+    jQuery('#BANCO').val(OPERACAO[0].BANCO1);
+    jQuery('#AGENCIA').val(OPERACAO[0].AGENCIA);
+    jQuery('#CONTA').val(OPERACAO[0].CONTA);
+    jQuery('#CPF').val(OPERACAO[0].CPF);
+    jQuery('#VALOR').val(OPERACAO[0].VALOR_TOTAL_OPERACAO);
+
+    jQuery('#DATA').val();
+    jQuery('#NUM_DOC').val();
+}
+
+function ClkConfirmarDeposito() {
+    ConfirmaDeposito();
+}
+
+function ConfirmaDeposito() {
+    if (OP_REALIZADA == '') {
+        var msg = ValidaDadosBancarios();
+
+
+
+        if (msg == '') {
+
+            var VALOR_PRODUTO = OPERACAO[0].VALOR_TOTAL_OPERACAO;
+            var ID_VENDA = OPERACAO[0].ID_VENDA;
+            var ID_STATUS_VENDA = 6;
+            var ID_TIPO_VENDA = OPERACAO[0].ID_TIPO_VENDA;
+
+
+            var BANCOS = jQuery('#BANCOS').val();
+            var AGENCIA = jQuery('#AGENCIA').val();
+            var CONTA = jQuery('#CONTA').val();
+            var DATA = jQuery('#DATA').val();
+            var NUM_DOC = jQuery('#NUM_DOC').val();
+            var ID_TIPO_DEPOSITO = jQuery('#DEPOSITO').val();
+
+
+            var ret = AlteraDepositoOperacao(ID_VENDA, COD_VENDA, ID_STATUS_VENDA, NUM_DOC, DATA, ID_TIPO_DEPOSITO, VALOR_PRODUTO, ID_TIPO_VENDA, null, ERROCONEXAO);
+            OP_REALIZADA = ret;
+            if (ret > 0) {
+
+                ExibeMensagem('Depósito informado com sucesso');
+            }
+        }
+        else {
+
+            ExibeMensagem(msg);
+        }
+    }
+    else {
+        ExibeMensagem("Este depósito já foi informado!")
+    }
+}
+
+function MontaSelect(OBJETO, CODIGO, NOME) {
+    jQuery('#' + OBJETO + '').append('<option value=' + CODIGO + '>' + NOME + '</option>');
+}
+
+
